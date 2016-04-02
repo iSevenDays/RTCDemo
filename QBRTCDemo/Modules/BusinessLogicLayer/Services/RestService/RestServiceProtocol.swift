795a8d8ae7ecec916217b28e8786cf48c09f859f
Anton Sokolchenko <wsevendays@gmail.com>
2016-04-02 17:12:32 +0300

    Added RestServiceProtocol

---
+++ QBRTCDemo/Modules/BusinessLogicLayer/Services/RestService/RestServiceProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  RestServiceProtocol.swift
+//  QBRTCDemo
+//
+//  Created by Anton Sokolchenko on 4/1/16.
+//  Copyright © 2016 anton. All rights reserved.
+//
+
+
+protocol RestServiceProtocol {
+	
+	func loginWithUser(user: SVUser, successBlock: ((user: SVUser) -> Void)?, errorBlock: ((NSError?) -> Void)?)
+	
+	func signUpWithUser(user: SVUser, successBlock: ((user: SVUser) -> Void)?, errorBlock: ((NSError?) -> Void)?)
+	
+}
