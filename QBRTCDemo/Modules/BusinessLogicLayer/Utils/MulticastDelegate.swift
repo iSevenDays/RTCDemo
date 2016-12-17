2c442c696800166db0ba740040b987012a2d1332
Anton Sokolchenko <wsevendays@gmail.com>
2016-12-17 02:08:39 +0200

    Fix for MulticastDelegate (https://github.com/tumtumtum/SwiftMulticastDelegate/pull/4)

--- QBRTCDemo/Modules/BusinessLogicLayer/Utils/MulticastDelegate.swift
+++ QBRTCDemo/Modules/BusinessLogicLayer/Utils/MulticastDelegate.swift
@@ -146,7 +146,7 @@ public class MulticastDelegate<T>
 					
 					if index > 0
 					{
-						newDelegates = Array(multicastDelegate.delegates[0..<index])
+						newDelegates = multicastDelegate.delegates.filter({$0.value != nil})
 					}
 				}
 			}
